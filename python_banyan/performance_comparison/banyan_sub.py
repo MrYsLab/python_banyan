9f3487466597849b4ea9da2ceb165c417e6b055f
MrYsLab <MrYsLab@gmail.com>
2019-02-13 17:43:22 -0500

    Version 3 Additions

--- python_banyan/performance_comparison/banyan_sub.py
+++ python_banyan/performance_comparison/banyan_sub.py
@@ -1,3 +1,21 @@
+"""
+ Copyright (c) 2016-2019 Alan Yorinks All right reserved.
+
+ Python Banyan is free software; you can redistribute it and/or
+ modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+ Version 3 as published by the Free Software Foundation; either
+ or (at your option) any later version.
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ General Public License for more details.
+
+ You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+ along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+"""
+
 import sys
 import time
 from python_banyan.banyan_base import BanyanBase
@@ -46,10 +64,6 @@ class BanyanSub(BanyanBase):
         data = payload['msg']
         self.message_count += 1
 
-        # time.sleep(.001)
-        # if not self.message_count % 100:
-        #     print(self.message_count)
-
         if data == 0:
             self.start = time.time()
 
@@ -61,5 +75,4 @@ class BanyanSub(BanyanBase):
             super(BanyanSub, self).clean_up()
             sys.exit(0)
 
-# instantiate this class
 BanyanSub()
--- python_banyan/performance_comparison/mqpub.py
+++ python_banyan/performance_comparison/mqpub.py
@@ -1,3 +1,21 @@
+"""
+ Copyright (c) 2016-2019 Alan Yorinks All right reserved.
+
+ Python Banyan is free software; you can redistribute it and/or
+ modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+ Version 3 as published by the Free Software Foundation; either
+ or (at your option) any later version.
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ General Public License for more details.
+
+ You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+ along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+"""
+
 import paho.mqtt.client as mqtt
 import socket
 import sys
@@ -10,6 +28,7 @@ class MQPUB(mqtt.Client):
         This class publishes 100000 "test" topic messages as fast as it can.
         It then prints the current time and exits.
         """
+
     def __init__(self):
         """
         This method connects, starts the loop and then publishes
@@ -41,7 +60,6 @@ class MQPUB(mqtt.Client):
         time.sleep(1)
         self.loop_stop()
         sys.exit(0)
-
 
 
 # instantiate the class
