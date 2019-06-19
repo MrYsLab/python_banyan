a0d530241165f4aed0fce730830dbca0cb0e187d
wwj718 <wuwenjie718@gmail.com>
2019-06-19 13:56:40 +0800

    unable to access 8.8.8.8 (in China)

--- python_banyan/backplane/backplane.py
+++ python_banyan/backplane/backplane.py
@@ -52,14 +52,7 @@ class BackPlane:
         """
 
         # get ip address of this machine
-
-        # create a socket
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-        # use the google dns to figure out the machine's address and use that address for the backplane.
-        # this precludes the necessity of having a network configuration file.
-        s.connect(('8.8.8.8', 0))
-        self.bp_ip_address = s.getsockname()[0]
+        self.bp_ip_address = socket.gethostbyname(socket.gethostname())
 
         print('\n******************************************')
         if backplane_name == "":
