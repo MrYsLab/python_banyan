a327e48820034515a793c502b95e418976db8a20
MrYsLab <MisterYsLab@gmail.com>
2024-11-28 11:34:42 -0500

    Force websockets to version 13.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('pypi_desc.md') as f:
 
 setup(
     name='python-banyan',
-    version='3.14',
+    version='3.15',
     packages=[
         'python_banyan',
         'python_banyan.banyan_base',
@@ -27,7 +27,7 @@ setup(
         'msgpack-numpy',
         'psutil',
         'apscheduler',
-        'websockets'
+        'websockets==13.1'
     ],
 
     entry_points={
